32789b0996dad37216a3dc76935d4b693f9e83b5
David.Bates <david.bates@ozmo.com>
2022-01-22 22:32:14 -0500

    Initial Commit

---
+++ Shared/WORDLERApp.swift
@@ -0,0 +1,17 @@
+//
+//  WORDLERApp.swift
+//  Shared
+//
+//  Created by David Bates on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct WORDLERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
